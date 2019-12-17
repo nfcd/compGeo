6315e31862c7966ab495a9df51381d42e30f96c7
AngelaChristinaHoch <53466354+AngelaChristinaHoch@users.noreply.github.com>
2019-12-17 20:08:06 +0100

    Update StCoordLine.py

--- resource/source/02_Chapter1/python_functions/StCoordLine.py
+++ resource/source/02_Chapter1/python_functions/StCoordLine.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 import math
+from ZeroTwoPi import ZeroTwoPi as ZeroTwoPi
 
 def StCoordLine(trd, plg, sttype):
     '''
@@ -15,8 +15,9 @@ def StCoordLine(trd, plg, sttype):
     NOTE: trend and plunge should be entered in radians
 
     StCoordLine uses function ZeroTwoPi
-    Python function translated from the Matlab function StCoordLine
-    of Structural Geology Algorithms by Allmendinger, Cardozo, & Fisher, 2011
+    
+    Python function translated from the Matlab function StCoordLine in the book:
+    Structural Geology Algorithms by Allmendinger, Cardozo, & Fisher, 2012
     '''
     
     # Take care of negative plunges
@@ -29,13 +30,11 @@ def StCoordLine(trd, plg, sttype):
     s2 = math.sqrt(2)
     plgS2 = plg/2
         
-    # Equal angle stereonet: From Equation 1.5 above
-    # Also see Pollard and Fletcher (2005), eq.2.72
+    # Equal angle stereonet
     if sttype == 0:
         xp = math.tan(piS4 - plgS2)*math.sin(trd)
         yp = math.tan(piS4 - plgS2)*math.cos(trd)
-    # Equal area stereonet: From Equation 1.6 above
-    # Also see Pollard and Fletcher (2005), eq.2.90
+    # Equal area stereonet
     elif sttype == 1:
         xp = s2*math.sin(piS4 - plgS2)*math.sin(trd)
         yp = s2*math.sin(piS4 - plgS2)*math.cos(trd)
